b240f137a42131540a21d2d35a239dc6f81b3017
Harshil <ghagrawal17@gmail.com>
2022-08-21 21:18:05 +0200

    ✨ Add more fields

--- nodes/n8nApi/N8nWorkflowDescription.ts
+++ nodes/n8nApi/N8nWorkflowDescription.ts
@@ -27,3 +27,34 @@ export const n8nWorkflowDescription: INodeProperties[] = [
 		],
 	},
 ];
+
+const getAllOperation: INodeProperties[] = [
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: ['getAll'],
+				resource: ['workflow'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Return Only Active Workflows',
+				name: 'activeWorkflows',
+				type: 'boolean',
+				default: false,
+			},
+			{
+				displayName: 'Tags',
+				name: 'tags',
+				type: 'string',
+				default: '',
+			},
+		],
+	},
+];
+
+export const n8nWorkflowFields: INodeProperties[] = [...getAllOperation];
