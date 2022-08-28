b99e99893f4690d246d5dbbdf6f0cd334d4d8698
Simon Hryszko <simon@hryszko.dev>
2022-08-28 15:48:52 +0200

    Make credential resource

---
+++ nodes/n8nApi/n8nCredentialDescription.ts
@@ -0,0 +1,67 @@
+import { INodeProperties } from 'n8n-workflow';
+
+export const n8nCredentialDescription: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		default: 'createCredential',
+		displayOptions: {
+			show: {
+				resource: ['credential'],
+			},
+		},
+		options: [
+			{
+				name: 'Create Credential',
+				value: 'createCredential',
+				action: 'Create credential',
+			},
+			{
+				name: 'Delete Credential',
+				value: 'deleteCredential',
+				action: 'Delete credential',
+			},
+			{
+				name: 'Get Credential Schema',
+				value: 'getCredentialSchema',
+				action: 'Get credential schema',
+			},
+		],
+	},
+];
+
+const getAllOperation: INodeProperties[] = [
+	{
+		displayName: 'Object',
+		name: 'object',
+		type: 'json',
+		default: '',
+		placeholder: '{ "name": "", "type": "", "data": { "token": "" } }',
+		description: 'Object with credential to be add',
+		typeOptions: {
+			alwaysOpenEditWindow: true,
+		},
+		displayOptions: {
+			show: {
+				'/operation': ['createCredential'],
+			},
+		},
+	},
+	{
+		displayName: 'Schema Name',
+		name: 'schemaName',
+		type: 'string',
+		default: '',
+		placeholder: 'freshdeskApi',
+		description: 'Name of the schema to use',
+		displayOptions: {
+			show: {
+				'/operation': ['getCredentialSchema'],
+			},
+		},
+	}
+];
+
+export const n8nCredentialFields: INodeProperties[] = [...getAllOperation];
