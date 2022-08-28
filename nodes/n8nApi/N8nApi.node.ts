4fd11f1028f07856326656fd070783cd18a0a975
Simon Hryszko <simon@hryszko.dev>
2022-08-28 15:20:54 +0200

    Rename workflowId to pathId

--- nodes/n8nApi/N8nApi.node.ts
+++ nodes/n8nApi/N8nApi.node.ts
@@ -298,7 +298,7 @@ export class N8nApi implements INodeType {
 
 			for (let i = 0; i < length; i++) {
 				// add other parameters (add value of parameter as array element)
-				['includeData', 'workflowId'].forEach((prop) => {
+				['includeData', 'pathId'].forEach((prop) => {
 					try {
 						qs[prop] = this.getNodeParameter(prop, i) as string;
 					} catch (error) {
@@ -326,9 +326,9 @@ export class N8nApi implements INodeType {
 
 				// Add the resource id to the endpoint
 				var endpointWithId = null;
-				if (qs.workflowId) {
-					endpointWithId = `${endpoint}/${qs.workflowId}`;
-					delete qs.workflowId;
+				if (qs.pathId) {
+					endpointWithId = `${endpoint}/${qs.pathId}`;
+					delete qs.pathId;
 				}
 
 				try {
